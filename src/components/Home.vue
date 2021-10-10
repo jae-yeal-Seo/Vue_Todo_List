c2910d4114d0c5c28999c2ae76f71c43c8c72523
woduf9173@naver.com <woduf9173@naver.com>
2021-10-10 17:41:39 +0900

    basic

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+<v-container>
+  {{ todoList }}
+  <v-layout row wrap>
+    <v-flex xs6>
+      <List/>
+    </v-flex>
+    <v-flex xs6>
+       <ListAdd @listAdd="listAdd"/>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+import List from './List'
+import ListAdd from './ListAdd'
+export default {
+  data(){
+    return {
+      todoList:[]
+    }
+  },
+  components:{
+    List,
+    ListAdd
+  },
+  methods:{
+    listAdd(memo){
+      this.todoList.push({ memo:memo, status:"created"})
+    }
+  }
+}
+</script>
---
+++ src/components/ListAdd.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+<v-textarea
+          outlined
+          v-model="memo"
+          label="Outlined textarea"
+          value=""
+        ></v-textarea> 
+<v-btn @click="listAdd">
+
+</v-btn>
+           
+</div>
+</template>
+<script>
+
+export default{
+  data(){
+      return {
+          memo:null
+      }
+  },
+  methods:{
+      listAdd(){
+         if(this.memo === null){
+             alert('먼저 할 일을 입력하세요');
+         } else{
+             this.$emit("listAdd",this.memo);
+         }
+      }
+  }
+}
+</script>
